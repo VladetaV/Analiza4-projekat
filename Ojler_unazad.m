3c121c9353426e1743239d00433f10432df7f9c0
VladetaV <vladetavujacic@gmail.com>
2024-08-28 09:34:24 +0200

    Ispravljen Ojler unazad

--- Ojler_unazad.m
+++ Ojler_unazad.m
@@ -4,18 +4,29 @@
 % pocetnu vrednost x0 i tacku do koje hocemo da plotujemo
 function [R, aproks_unazad] = Ojler_unazad(f, t0, x0, t_max)
 
+% Simboličke promenljive za analitičko rešenje
+syms x(t_sym) t_sym
+Dy = diff(x, t_sym);  % Prva derivacija x po t
+ode = Dy == f(t_sym, x); % Diferencijalna jednačina
+cond = x(t0) == x0; % Početni uslov
+analiticko_resenje = dsolve(ode, cond); % Analitičko rešenje
+
+% Pretvaranje analitičkog rešenja u funkciju
+analiticko_resenje_funkcija = matlabFunction(analiticko_resenje);
+
+
 % zadajemo korak h (konfigurabilno)
 h = 0.01;
 t = t0;
 y = x0;
 aproks_unazad = x0;
-aproks_unapred = x0;
 t_osa = t0:h:t_max;
 T = length(t_osa);
 
 while(length(aproks_unazad) < T)
-    % prvo izracunavamo x(t+h) pomocu Ojlera unapred
-    y = y + h*f(t,y);
+    % prvo izracunavamo x(t+h) pomocu analitickog resenja dobijenog
+    % pomocu dsolve matlab f-je
+    y = analiticko_resenje_funkcija(t+h);
     yp = y + h*f(t+h,y);
     aproks_unazad = [aproks_unazad, yp];
     t = t + h;
@@ -30,8 +41,9 @@ aproks_unazad_runge = x0;
 t_osa = t0:h:t_max;
 T = length(t_osa);
 while(length(aproks_unazad_runge)<T)
-    % prvo izracunavamo x(t+h) pomocu Ojlera unapred
-    y = y + h*f(t,y);
+    % prvo izracunavamo x(t+h) pomocu analitickog resenja dobijenog
+    % pomocu dsolve matlab f-je
+    y = analiticko_resenje_funkcija(t+h);
     yp = y + h*f(t+h,y);
     aproks_unazad_runge = [aproks_unazad_runge yp];
     t = t + h;
